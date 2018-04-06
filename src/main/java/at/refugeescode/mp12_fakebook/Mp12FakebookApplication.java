3b72cf6fa0a55b7c0cbb290c210eafd4de045f09
Fozat Kardouh <fozatkardouh@gmail.com>
2018-04-06 13:50:20 +0200

    added empty project

---
+++ src/main/java/at/refugeescode/mp12_fakebook/Mp12FakebookApplication.java
@@ -0,0 +1,12 @@
+package at.refugeescode.mp12_fakebook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mp12FakebookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mp12FakebookApplication.class, args);
+	}
+}
